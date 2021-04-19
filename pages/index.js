0e8b8b73a030f938a5f1915a529bd002a8c36612
Thomas Green <thomaspetergreen@gmail.com>
2021-04-19 18:19:52 +1000

    styling and typo

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,12 @@ const Home = () => (
       <title>Thomas Green</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="preconnect" href="https://fonts.gstatic.com"/>
-      <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:wght@300&display=swap" rel="stylesheet"/>
+      <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:wght@300;400;500;700&display=swap" rel="stylesheet"/>
     </Head>
     <h1 className="sr-only">Thomas Green</h1>
     <div className="intro-content">
       <p>Hey I’m Thomas Green, a digital product designer living in Melbourne Australia.</p>
-      <p>I’m interested in desiging technologies and systems for human needs.</p>
+      <p>I’m interested in designing technologies and systems for human needs.</p>
       <p>I currently work at <a href="https://www.telstrahealth.com/">Telstra Health</a>.</p>
     </div>
     <style jsx>{`
@@ -22,7 +22,8 @@ const Home = () => (
       }
 
       :global(body) {
-        font-size: calc(1rem + 1.2vw);
+        font-size: 1rem;
+        font-weight: 500;
         line-height: 1.5;
         font-family: 'Alegreya Sans', sans-serif;
         -webkit-font-smoothing: antialiased;
@@ -37,9 +38,18 @@ const Home = () => (
         text-decoration: none;
       }
       .intro-content {
-        padding: 4vmax;
-        max-width: 30rem;
+        font-size: 1rem;
+        padding: 6vw 10vw;
+        max-width: 17em;
       }
+      @media (min-width: 1024px) {
+        .intro-content {
+          font-size: 26px;
+          padding: 4vmax;
+          max-width: 17em;
+        }
+       }
+
       .sr-only {
         border: 0 !important;
         clip: rect(1px, 1px, 1px, 1px) !important; /* 1 */
